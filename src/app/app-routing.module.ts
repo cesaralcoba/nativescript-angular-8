caaa0f5cf73648807c4cabd342eeb2951b96b8d5
cesar alcoba <calcobac@everis.com>
2020-04-23 22:29:45 -0400

    dependency injection

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
+import {PersonsComponent} from './persons/persons.component'
+import {PersonInputComponent} from './persons/person-input.componet'
+
+const routes: Routes = [
+  {path:'', component: PersonsComponent},
+  {path:'input', component: PersonInputComponent}
+];
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule{
+
+}
